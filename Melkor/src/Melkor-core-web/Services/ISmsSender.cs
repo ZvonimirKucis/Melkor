f1894d5443bf749be8f40aeaef48c3bc51b40fee
Alen Štruklec <m3talen@gmail.com>
2016-12-10 11:19:17 +0100

    Added Melkor-core-web

---
+++ Melkor/src/Melkor-core-web/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Melkor_core_web.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
