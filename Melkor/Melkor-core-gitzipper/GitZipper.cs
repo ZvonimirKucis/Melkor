e0afdcc5bc1c716d3178011cf00485b186ecf59c
ZvonimirKucis <zvonimirkucis@gmail.com>
2016-12-17 12:53:51 +0100

    GitZipper added

---
+++ Melkor/Melkor-core-gitzipper/GitZipper.cs
@@ -0,0 +1,62 @@
+﻿using Ionic.Zip;
+using System;
+using System.IO;
+using System.Net;
+
+namespace Melkor_core_gitzipper
+{
+    /// <summary>
+    ///     <p>Autor : Kucis</p>
+    /// </summary>
+    public class GitZipper
+    {
+        private readonly string _extension = "/archive/master.zip";
+        private readonly string _url;
+        private string _directoryLocation;
+        private string _downloadFile;
+        private readonly string _userGuid;
+
+        public GitZipper(string url, string userGuid)
+        {
+            this._url = url;
+            this._userGuid = userGuid;
+        }
+
+        public void GitDownload(string directoryLocation)
+        {
+            _directoryLocation = $@"{directoryLocation}{_userGuid}\";
+            _downloadFile = $"{directoryLocation}{_userGuid}master.zip";
+            
+            Directory.CreateDirectory(_directoryLocation);
+           
+            string downloadLink = _url + _extension;
+            using (WebClient client = new WebClient())
+            {
+                Console.WriteLine($"Downloading {downloadLink}");
+                client.DownloadFile(new Uri(downloadLink), _downloadFile);
+            }
+        }
+
+        public static void CleanUp(string directoryLocation)
+        {
+            if (Directory.Exists(directoryLocation))
+                Directory.Delete(directoryLocation, true);
+        }
+
+        /// <summary>
+        /// <p>Unzips the downloaded zip</p>
+        /// Edited (14.12.2016) - @M3talen : Azure support.
+        /// </summary>
+        public void GitUnzip()
+        {
+            Console.WriteLine($"Unziping {_downloadFile} to {_directoryLocation}");
+            using (ZipFile zip = ZipFile.Read(_downloadFile))
+            {
+                zip.ExtractAll(_directoryLocation, ExtractExistingFileAction.OverwriteSilently);
+            }
+
+            File.Delete(_downloadFile);
+        }
+
+    }
+}
